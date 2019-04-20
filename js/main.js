4b9846f5f6a25e6557178fb43256b799c6237c42
William Gagne <williamgagnepals@gmail.com>
2019-04-20 10:40:21 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var main = {
+    'timerDrop' : null,
+    'timerClean' : null,
+    'sky' : null
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function deleteDroppedDrops(){
+    var drops = document.getElementsByClassName("drop");
+    while(drops.length >= 30){
+        main.sky.removeChild(drops[0]);
+    }
+}
+
+function createDroplet(){
+    var drop = document.createElement("img");
+    var width = window.innerWidth;
+    drop.src="assets/img/raindrop.svg";
+    drop.className = "drop";
+    var pos = getRandomInt(0, width)-50;
+    drop.style.left = pos+"px";
+    main.sky.appendChild(drop);
+    deleteDroppedDrops();
+}
+
+function init(){
+    main.sky = document.getElementById("main");
+    main.timerDrop = setInterval(createDroplet, 100);
+}
+window.addEventListener("load", init, false);
